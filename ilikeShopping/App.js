a28a6ef482e5e2a559cf9005940653e0ea0a776b
Heemaeng <gml7814@gmail.com>
2021-10-07 23:52:13 +0900

    navigation 화면이동

--- ilikeShopping/App.js
+++ ilikeShopping/App.js
@@ -1,28 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import * as React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import bottomNav from './components/bottomNav';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+function Home() {
+  return (
+    <View style={styles.eachView}>
+      <Text> 홈 화면 입니다.</Text>
+      <Button
+        title="쇼핑 카트 화면으로 가기"
+        onPress={() => this.props.navigation.navigate('ShoppingCart')}
+      />
+      <Button
+        title="설정 화면으로 가기"
+        onPress={() => this.props.navigation.navigate('Setting')}
+      />
+    </View>
+  );
+}
+function ShoppingCart() {
+  return (
+    <View style={styles.eachView}>
+      <Text> 쇼핑 카트 화면 입니다.</Text>
+      <Button
+        title="홈 화면으로 가기"
+        onPress={() => this.props.navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+function Setting() {
+  return (
+    <View style={styles.eachView}>
+      <Text> 설정 화면 입니다.</Text>
+      <Button
+        title="홈 화면으로 가기"
+        onPress={() => this.props.navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+const App = createStackNavigator(
+  {
+    ShoppingCart: {
+      screen: ShoppingCart,
+    },
+    Home: {
+      screen: Home,
+    },
+    Setting,
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
+
+const AppContainer = createAppContainer(App);
 
 function ilikeShopping() {
   setTimeout(() => {
     SplashScreen.hide();
   }, 500);
 
-  return (
-    <>
-      <View>
-        <Text>Hello World</Text>
-      </View>
-      <bottomNav />
-    </>
-  );
+  return <AppContainer />;
 }
+
+const styles = StyleSheet.create({
+  centerView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  eachView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default ilikeShopping;
