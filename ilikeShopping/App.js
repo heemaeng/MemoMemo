6e73ea8f58bd5aa1f6a44227e817760e1852db87
Heemaeng <gml7814@gmail.com>
2021-10-11 21:42:48 +0900

    주석 삭제

--- ilikeShopping/App.js
+++ ilikeShopping/App.js
@@ -1,85 +1,3 @@
-// import * as React from 'react';
-// import {View, Text, StyleSheet, Button} from 'react-native';
-// import SplashScreen from 'react-native-splash-screen';
-// import {createAppContainer} from 'react-navigation';
-// import {createStackNavigator} from 'react-navigation-stack';
-
-// function Home(props) {
-//   return (
-//     <View style={styles.eachView}>
-//       <Text> 홈 화면 입니다.</Text>
-//       <Button
-//         title="쇼핑 카트 화면으로 가기"
-//         onPress={() => props.navigation.navigate('ShoppingCart')}
-//       />
-//       <Button
-//         title="설정 화면으로 가기"
-//         onPress={() => props.navigation.navigate('Setting')}
-//       />
-//     </View>
-//   );
-// }
-// function ShoppingCart(props) {
-//   return (
-//     <View style={styles.eachView}>
-//       <Text> 쇼핑 카트 화면 입니다.</Text>
-//       <Button
-//         title="홈 화면으로 가기"
-//         onPress={() => props.navigation.navigate('Home')}
-//       />
-//     </View>
-//   );
-// }
-
-// function Setting(props) {
-//   return (
-//     <View style={styles.eachView}>
-//       <Text> 설정 화면 입니다.</Text>
-//       <Button
-//         title="홈 화면으로 가기"
-//         onPress={() => props.navigation.navigate('Home')}
-//       />
-//     </View>
-//   );
-// }
-
-// const App = createStackNavigator(
-//   {
-//     ShoppingCart: {
-//       screen: ShoppingCart,
-//     },
-//     Home: {
-//       screen: Home,
-//     },
-//     Setting,
-//   },
-//   {
-//     initialRouteName: 'Home',
-//   },
-// );
-
-// const AppContainer = createAppContainer(App);
-
-// function ilikeShopping() {
-//   setTimeout(() => {
-//     SplashScreen.hide();
-//   }, 500);
-
-//   return <AppContainer />;
-// }
-
-// const styles = StyleSheet.create({
-//   centerView: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   eachView: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import HomeScreen from './components/HomeScreen';
@@ -88,7 +6,6 @@ import SettingScreen from './components/SettingScreen';
 import StorageScreen from './components/StorageScreen';
 import SplashScreen from 'react-native-splash-screen';
 
-// export default createAppContainer(TabNavigator);
 export default function App() {
   const TabNavigator = createBottomTabNavigator({
     Home: {
