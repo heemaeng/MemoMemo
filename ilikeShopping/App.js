5d74ea48f6c843edb1c3d01e0c8c7bd78dd99a83
Heemaeng <gml7814@gmail.com>
2021-10-11 23:52:47 +0900

    초기 화면에 navigation 배치

--- ilikeShopping/App.js
+++ ilikeShopping/App.js
@@ -1,30 +1,33 @@
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
-import HomeScreen from './components/HomeScreen';
-import ShoppingCartScreen from './components/ShoppingCartScreen';
-import SettingScreen from './components/SettingScreen';
-import StorageScreen from './components/StorageScreen';
+import * as React from 'react';
+import {Text, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from './components/HomeScreen';
+import SettingsScreen from './components/SettingsScreen';
+import ShoppingCartScreen from './components/ShoppingCartScreen';
+import StorageScreen from './components/StorageScreen';
+
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="ShoppingCart" component={ShoppingCartScreen} />
+      <Tab.Screen name="Storage" component={StorageScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
-  const TabNavigator = createBottomTabNavigator({
-    Home: {
-      screen: HomeScreen,
-    },
-    ShoppingCart: {
-      screen: ShoppingCartScreen,
-    },
-    Storage: {
-      screen: StorageScreen,
-    },
-    Setting: {
-      screen: SettingScreen,
-    },
-  });
-
   setTimeout(() => {
     SplashScreen.hide();
   }, 500);
-
-  return createAppContainer(TabNavigator);
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
