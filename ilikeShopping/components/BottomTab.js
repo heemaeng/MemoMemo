485d62278a8d20e46a93a66b0b2f46dec48065c3
Heemaeng <gml7814@gmail.com>
2021-10-25 23:40:18 +0900

    navigation tab 생성
    4개의 탭에 각각 아이콘 넣음

--- ilikeShopping/components/BottomTab.js
+++ ilikeShopping/components/BottomTab.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 import HomeScreen from './HomeScreen';
 import SettingsScreen from './SettingsScreen';
 import ShoppingCartScreen from './ShoppingCartScreen';
@@ -9,11 +10,56 @@ const Tab = createBottomTabNavigator();
 
 export function Tabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="ShoppingCart" component={ShoppingCartScreen} />
-      <Tab.Screen name="Storage" component={StorageScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: true,
+      }}
+      activeColor="#000">
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tarBarLabel: 'Home',
+          tabBarIcon: ({color}) => (
+            <Icon name="home-outline" color={color} size={28} />
+          ),
+          headerTitleAlign: 'center',
+          headerTintColor: '#000',
+        }}
+      />
+      <Tab.Screen
+        name="ShoppingCart"
+        component={ShoppingCartScreen}
+        options={{
+          tabBarLabel: 'ShoppingCart',
+          tabBarIcon: ({color}) => (
+            <Icon name="cart-outline" color={color} size={28} />
+          ),
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Tab.Screen
+        name="Storage"
+        component={StorageScreen}
+        options={{
+          tabBarLabel: 'Storage',
+          tabBarIcon: ({color}) => (
+            <Icon name="reader-outline" color={color} size={28} />
+          ),
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({color}) => (
+            <Icon name="options-outline" color={color} size={28} />
+          ),
+          headerTitleAlign: 'center',
+        }}
+      />
     </Tab.Navigator>
   );
 }
