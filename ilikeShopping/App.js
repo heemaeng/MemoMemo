fec9b65d9db4605e9ec77ca0f4ffa6335830930b
Heemaeng <gml7814@gmail.com>
2021-10-25 23:38:06 +0900

    SafeAreaView create

--- ilikeShopping/App.js
+++ ilikeShopping/App.js
@@ -1,15 +1,23 @@
 import * as React from 'react';
+import {StatusBar} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {NavigationContainer} from '@react-navigation/native';
 import {Tabs} from './components/BottomTab';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import SafeViewAndroid from './components/SafeViewAndroid';
 
 export default function App() {
   setTimeout(() => {
     SplashScreen.hide();
   }, 500);
+  StatusBar.setBackgroundColor('#e9ecef');
+  // StatusBar.setTranslucent(true);
+  StatusBar.setBarStyle('dark-content');
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+      <NavigationContainer>
+        <Tabs />
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
