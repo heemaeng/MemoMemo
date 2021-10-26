18a16b0bed99336f8a782540dfef4fa17ce6800c
Heemaeng <gml7814@gmail.com>
2021-10-26 22:48:34 +0900

    스크롤뷰 추가
    HomeScreen 화면에 스크롤뷰 추가하여 스크롤 가능함

--- ilikeShopping/components/HomeScreen.js
+++ ilikeShopping/components/HomeScreen.js
@@ -1,72 +1,73 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 
 export default function HomeScreen() {
   return (
     <>
-      <View
-        style={{
-          flexDirection: 'row',
-          height: 100,
-          padding: 20,
-        }}>
-        <View style={{backgroundColor: 'blue', flex: 0.3}} />
-        <Text>홈 화면 입니다.</Text>
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          height: 100,
-          padding: 20,
-        }}>
-        <View style={{backgroundColor: 'blue', flex: 0.3}} />
-        <Text>홈 화면 입니다.</Text>
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          height: 100,
-          padding: 20,
-        }}>
-        <View style={{backgroundColor: 'blue', flex: 0.3}} />
-        <Text>홈 화면 입니다.</Text>
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          height: 100,
-          padding: 20,
-        }}>
-        <View style={{backgroundColor: 'blue', flex: 0.3}} />
-        <Text>홈 화면 입니다.</Text>
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          height: 100,
-          padding: 20,
-        }}>
-        <View style={{backgroundColor: 'blue', flex: 0.3}} />
-        <Text>홈 화면 입니다.</Text>
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          height: 100,
-          padding: 20,
-        }}>
-        <View style={{backgroundColor: 'blue', flex: 0.3}} />
-        <Text>홈 화면 입니다.</Text>
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          height: 100,
-          padding: 20,
-        }}>
-        <View style={{backgroundColor: 'blue', flex: 0.3}} />
-        <Text>홈 화면 입니다.</Text>
-      </View>
+      <ScrollView style={styles.coinView}>
+        <View
+          style={{
+            flexDirection: 'row',
+            height: 100,
+            padding: 5,
+          }}>
+          <View style={{backgroundColor: 'blue', flex: 1}} />
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            height: 100,
+            padding: 20,
+          }}>
+          <View style={{backgroundColor: 'blue', flex: 0.3}} />
+          <Text>홈 화면 입니다.</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            height: 100,
+            padding: 20,
+          }}>
+          <View style={{backgroundColor: 'blue', flex: 0.3}} />
+          <Text>홈 화면 입니다.</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            height: 100,
+            padding: 20,
+          }}>
+          <View style={{backgroundColor: 'blue', flex: 0.3}} />
+          <Text>홈 화면 입니다.</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            height: 100,
+            padding: 20,
+          }}>
+          <View style={{backgroundColor: 'blue', flex: 0.3}} />
+          <Text>홈 화면 입니다.</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            height: 100,
+            padding: 20,
+          }}>
+          <View style={{backgroundColor: 'blue', flex: 0.3}} />
+          <Text>홈 화면 입니다.</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            height: 100,
+            padding: 20,
+          }}>
+          <View style={{backgroundColor: 'blue', flex: 0.3}} />
+          <Text>홈 화면 입니다.</Text>
+        </View>
+      </ScrollView>
     </>
   );
 }
@@ -76,5 +77,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  coinView: {
+    width: '100%',
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'transparent',
   },
 });
