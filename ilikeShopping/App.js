413e4453ce8622f3649438e77dd43f20e5996cbf
Heemaeng <gml7814@gmail.com>
2021-12-07 23:56:55 +0900

    파일 위치 변경, 스타일 변경 - 색상 변경

--- ilikeShopping/App.js
+++ ilikeShopping/App.js
@@ -2,15 +2,15 @@ import * as React from 'react';
 import {StatusBar} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {NavigationContainer} from '@react-navigation/native';
-import {Tabs} from './components/BottomTab';
+import {Tabs} from './modules/BottomTab';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import SafeViewAndroid from './components/SafeViewAndroid';
+import SafeViewAndroid from './modules/SafeViewAndroid';
 
 export default function App() {
   setTimeout(() => {
     SplashScreen.hide();
   }, 500);
-  StatusBar.setBackgroundColor('#e9ecef');
+  StatusBar.setBackgroundColor('#bec5b7');
   // StatusBar.setTranslucent(true);
   StatusBar.setBarStyle('dark-content');
   return (
