19300d0485009576f360af1d9ed2dee436fb7763
Heemaeng <gml7814@gmail.com>
2021-12-07 23:55:29 +0900

    파일 위치 변경 - 화면, 모듈

--- ilikeShopping/components/SafeViewAndroid.js
+++
@@ -1,9 +0,0 @@
-import {StyleSheet, Platform, StatusBar} from 'react-native';
-// android SafeArea
-export default StyleSheet.create({
-  AndroidSafeArea: {
-    flex: 1,
-    backgroundColor: 'red',
-    paddingTop: Platform.OS === 'android' ? -StatusBar.currentHeight : 0,
-  },
-});
