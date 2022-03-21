e211f5b6f286fd9a6cad2605e02c4ce3c2a107bf
Heemaeng <gml7814@gmail.com>
2022-03-21 20:53:55 +0900

    스플래시 스크린 미작동

--- MemoMemo/App.js
+++ MemoMemo/App.js
@@ -6,9 +6,9 @@ import AppNavigator from './src/navigation/AppNavigator';
 import {StatusBar} from 'react-native';
 
 export default function App() {
-  setTimeout(() => {
-    SplashScreen.hide();
-  }, 500);
+  // setTimeout(() => {
+  //   SplashScreen.hide();
+  // }, 500);
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
       <AppNavigator />
--- MemoMemo/android/app/src/main/java/com/memomemo/MainActivity.java
+++ MemoMemo/android/app/src/main/java/com/memomemo/MainActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
-  @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      SplashScreen.show(this, true);
-      super.onCreate(savedInstanceState);
-    }
+  // @Override
+  //   protected void onCreate(Bundle savedInstanceState) {
+  //     SplashScreen.show(this, true);
+  //     super.onCreate(savedInstanceState);
+  //   }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
